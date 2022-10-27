72a617b6eb5769dd2a084e39e27462c367f6a0ec
Michaelcc28 <109257666+Michaelcc28@users.noreply.github.com>
2022-10-26 22:50:11 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const SUUUUUUBtn = document.querySelector("#SUUUUUUBtn");
+
+/*SUUUUUUBtn.style.background = "pink";*/
+
+SUUUUUUBtn.addEventListener("click",function () {
+    alert("Como se que a la primera pusiste que SUUUUUU ahora te lees todo lo que tengo que decir (MODO ROMEO) antes siento que te lo pedi muy rapido y es que como sabes soy muy emociado jajaja pero aun asi 1 mes no es mucho para conocerte por completo pero siento que te conozco de toda la vida y aun que no lo paresca te pareces mucho a mi de como has vivido tu infancia y como eres ahora aun falta mucho por contar de parte tuya y mia pero bueno como dijo el DUKO ¨NO TENGAS MIEDO BABY ESTO RECIEN EMPIEZA¨ que trato decirte con esto pues ¨ lo que quiero decir con estoes que me des una oportunidad y veras como te dare todo el amor comprencion ternura y cofianza que nesecitas (no puse comas por que este programa detecta las comas como algoritmos xd) bueno volviendo al tema no te prometo ser pefecto por que creo que eso no existe pero lo que si te prometo es ser el chico que siempre este para ti y nunca dejarte sola y apoyarte en todo y por encima de todo verte triunfar y que cumplas tus metas y si puedo estar en ellas pues seria muy feliz y pues bueno creo que me olvido de algunas cosas de escribir pero es que no prepare esto lo que te digo sale de mi corazon de pollo jajaj y pues quiero que seas sincera con tu respuesta aun que se que el programa esta hecho para que dias suuuuuuu pero es que la bromita jajajjaa pero bueno eso mi chiquita te quiero mucho y espero poder ser tu novio y vivir muchas experencias juntos :3   ")
+
+});
+
+const NOOOOBtn = document.querySelector("#NOOOOBtn");
+
+NOOOOBtn.addEventListener("mouseover", function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    NOOOOBtn.style.setProperty("top",randomY+"%");
+    NOOOOBtn.style.setProperty("left",randomX+"%");
+    NOOOOBtn.style.setProperty("transform",`translate(-${randomX}%,-${randomY}%)`);
+
+
+})
+
+
+
+
+
